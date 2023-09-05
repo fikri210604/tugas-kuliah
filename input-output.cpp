ac3c6e0d533c407bc5ee627f873cf346ff635086
Ahmad Fikri Hanif <afh.fikri2106@gmail.com>
2023-09-05 20:52:11 +0700

    Tugas DDP 1

---
+++ input-output.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string a,b,c;
+
+    cout << "Nama : ";
+    cin >> a;
+    cout << "Kelas : ";
+    cin >> b;
+    cout << "NPM : ";
+    cin >> c;
+
+    cout << endl << endl;
+    cout << "Nama : " << a << endl;
+    cout << "Kelas : " << b << endl;
+    cout << "NPM : " << c << endl;
+
+    return 0;
+}
